619016cc525aafa16740e2841a4b9dd66d6634f8
KevinAlejandroParra <luisparra5380@gmail.com>
2024-11-09 23:53:05 -0500

    initialization of the project with tailwind and deisy iu included

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+function App() {
+return (<div className="App">
+    <navbar>
+  <Navbar/>
+  </navbar>
+    </div>
+      
+
+      
+    
+  )
+}
+
+export default App

9c6c170f544b1fc56f1b2920a2a59351759035cf
KevinAlejandroParra <luisparra5380@gmail.com>
2024-11-10 23:52:48 -0500

    feature: new customs about themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
+import Hero from './components/Hero';
 function App() {
-return (<div className="App">
-    <navbar>
-  <Navbar/>
-  </navbar>
-    </div>
-      
+  return (
+    <div className="App">
+      <Navbar />
 
-      
-    
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+

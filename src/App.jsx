31e8339df021c9e586ab82be07d1ac733d56696f
KevinAlejandroParra <luisparra5380@gmail.com>
2024-11-18 23:56:45 -0500

    accesibility widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CustomNavbar from "./components/Navbar.jsx";
 import { Banner } from "./components/Banner.jsx";
+import { AccessibilityWidget } from "sena-accessibility";
 function App() {
   return (
     <>
       <CustomNavbar />
       <Banner/>
+      <AccessibilityWidget theme="ligth" />
    
     </>
   );

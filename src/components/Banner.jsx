639e2b04da63084b86c8331e30c974da39f8acc4
KevinAlejandroParra <luisparra5380@gmail.com>
2024-11-20 23:47:56 -0500

    fix: filter shadow for banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,23 +1,26 @@
 export const Banner = () => {
   return (
     
-    <section className="dark:bg-[#061E2D] light:bg-[#d8693e] min-h-screen flex items-center" id="home">
+    <section className="dark:bg-[#061E2D] bg-white min-h-screen flex items-center" id="home">
       <div className="container mx-auto px-4">
-  <div className="fixed w-56 h-56 bg-white bottom-32 left-1/3 rounded-full filter blur-[100px] opacity-30 animate-blob animation-delay-4000"></div>
         <div className="flex flex-col lg:flex-row items-center justify-between">
           <div className="lg:w-1/2 lg:pr-12 space-y-6">
-            <h2 className="text-[#F86D31] text-xl font-bold">
-              Classment Academy
-            </h2>
-            <h1 className="text-5xl md:text-6xl font-bold leading-tight">
+            <p className="text-[#F86D31] text-3xl font-bold">
+            Classment Academy
+            </p>
+            <h1 className="text-7xl md:text-6xl font-bold leading-tight">
+            <div className="fixed w-56 h-56 dark:bg-white bg-[#f0ae333e] inset-x-96 top-90 h-16  rounded-full filter blur-[100px] dark:opacity-30  animate-blob animation-delay-4000"></div>
+
               Forjando Grandes{" "}
-              <span className="text-5xl text-gradient">deportistas</span>{" "}
-              desde cualquier lugar!
+              <br></br>
+              <span className="text-7xl text-gradient">deportistas</span>{" "}
+              desde <br></br>cualquier lugar!
             </h1>
-            <p className="text-base leading-relaxed max-w-lg">
+            <p className="text-base text-xl leading-relaxed max-w-lg">
               Desarrolla tu potencial deportivo con nuestra plataforma de 
               entrenamiento personalizado y acondicionamiento físico.
             </p>
+            <div className="flex space-x-4">
             <button 
               className="bg-[#F86D31] hover:bg-[#071822] text-white font-bold py-3 px-6 rounded-full transition-colors duration-300"
               onClick={() => console.log('connect')}
@@ -25,9 +28,17 @@ export const Banner = () => {
               Conectar con nosotros
               <span className="text-sm pl-2">🚀</span>
             </button>
+            <button 
+              className="bg-[#F86D31] hover:bg-[#071822] text-white font-bold py-3 px-6 rounded-full transition-colors duration-300"
+              onClick={() => console.log('connect')}
+            >
+              Ver detalles 
+              <span className="text-sm pl-2">🦁</span>
+            </button>
+            </div>
           </div>
 
-          <div className="lg:w-1/2 mt-8 lg:mt-0">
+          <div className="lg:w-1/2 mt-8 lg:mt-0 dark:drop-shadow-[0_0_8px_#FFFFFF] drop-shadow-[0_0_8px_#F86D31] ">
             <img 
               src="/images/basquetbolista.png" 
               alt="Basquetbolista"
@@ -37,5 +48,5 @@ export const Banner = () => {
         </div>
       </div>
     </section>
-  )
+)
 }

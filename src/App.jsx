e45b8c338057ff54f0c6136202ae59a0d15cfbe8
KevinAlejandroParra <luisparra5380@gmail.com>
2024-11-23 23:52:01 -0500

    feature carrousel section in process

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import CustomNavbar from "./components/Navbar.jsx";
 import { Banner } from "./components/Banner.jsx";
 import { AccessibilityWidget } from "sena-accessibility";
+import { Carrousel } from "./components/Carrousel.jsx";
 function App() {
   return (
     <>
       <CustomNavbar />
       <Banner/>
       <AccessibilityWidget theme="ligth" />
+      <Carrousel />
    
     </>
   );
---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,14 @@
+export const Carrousel = () => {
+    return (
+   
+   
+   <>
+   <div class="relative overflow-hidden py-16 bg-ghost" id="title-courses">
+      <div class="container mx-auto px-4 space-y-5">
+        <h2 class="text-4xl font-extrabold text-center capitalize">Cursos destacados</h2>
+     
+      </div>
+    </div>
+    </>
+    );
+}

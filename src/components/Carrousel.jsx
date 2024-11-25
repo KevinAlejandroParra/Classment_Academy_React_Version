d3a79ad0f1a32bff21a8d92226ec089dbd6b69c0
KevinAlejandroParra <luisparra5380@gmail.com>
2024-11-24 22:40:07 -0500

    fix: carrousel in process

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,9 +3,10 @@ export const Carrousel = () => {
    
    
    <>
-   <div class="relative overflow-hidden py-16 bg-ghost" id="title-courses">
+   
+   <div class="dark:bg-[#061E2D] bg-white relative overflow-hidden py-16 bg-ghost" id="title-courses">
       <div class="container mx-auto px-4 space-y-5">
-        <h2 class="text-4xl font-extrabold text-center capitalize">Cursos destacados</h2>
+        <h2 class="dark:text-white text-gray-800 text-4xl font-extrabold text-center capitalize">Cursos destacados</h2>
      
       </div>
     </div>

c5de0d07eea5d174fd3a51e0a59942b302dd35cf
Sneither <danielbernalg.04@gmail.com>
2025-04-01 22:05:56 -0500

    feature: school information (90%)

--- src/components/SchoolDetail.jsx
+++ src/components/SchoolDetail.jsx
@@ -12,7 +12,8 @@ import {
   faPhone,
   faClock,
   faLocationDot,
-  faChalkboardUser
+  faChalkboardUser,
+  faUser
 } from '@fortawesome/free-solid-svg-icons';
 import CourseModel from '../models/CourseModel';
 
@@ -37,3 +38,93 @@ const SchoolHeader = ({School}) => (
     </figure>
 );
 
+const SchoolDetail = () => {
+    const [school, setSchool] = useState(null);
+    const[loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchSchoolDetails = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/api/escuelas/${id}');
+                const SchoolData = await response.json();
+                SetSchool(SchoolData);
+            } catch(error){
+                setError('Error al cargar los detalles de la escuela. Por favor, intente más tarde.');
+            } finally {
+                setLoading(false);
+            }
+    };
+
+    fetchSchoolDetails();
+}, [id]);
+
+if (loading) {
+    return <div className="text-center py-8 text-light-primary dark:text-dark-primary">Cargando...</div>;
+}
+
+if (error) {
+    return <div className="text-center py-8 text-red-500">{error}</div>;
+}
+
+if(!school){
+    return <div className='text-center py-8 text-light-primary dark:text-dark-primary'>Escuela no encontrada</div>
+}
+
+return (
+
+    <div className='"min-h-screen bg-light-background dark:bg-dark-background'>
+        <SchoolHeader School={school}/>
+
+        <main className='"container mx-auto px-4 max-w-6xl -mt-16 relative z-10'>
+            <div className='bg-white dark:bg-dark-card rounded-3xl shadow-2xl shadow-light-shadow dark:shadow-dark-shadow overflow-hidden'>
+                <div className='p-8 space-y-8'>
+                        {/*Informacion de la escuela*/}
+                        <section>
+                            <div className='flex flex-wrap justify-between items-center mb-6'>
+                                <div>
+                                    <h2 className='text-3xl font-bold text-light-primary dark:text-dark-primary mb-2'>
+                                        <FontAwesomeIcon icon = {faSchool} className='mr-3 text-light-accent dark:text-dark-accent'/>
+                                        {school.escuela.nombre}
+                                    </h2>
+
+                                    <p className='text-light-secondary dark:text-dark-secondary'>
+                                        {school.escuela.direccion || 'Direccion no disponible por el momento'}
+                                    </p>
+                                </div>
+
+                                <div className='text-right'>
+                                    <p className='text-3xl font-bold text-light-accent dark:text-dark-accent mb-2'>
+                                        {shchool.escuela.telefono || 'Número de telefono no dispnible por el momento'}
+                                    </p>
+                                    <button className='btn bg-light-success dark:bg-dark-success text-white hover:bg-opacity-90 rounded-full px-6 py-2'>
+                                        <FontAwesomeIcon icon = {faUser} className='mr-2'/>
+                                        Incribirme
+                                    </button>
+                                </div>
+                            </div>
+                        </section>
+
+                        {/*Descripcion*/}
+
+                        <section>
+                            <h3 className='text-2xl font-semibold text-light-primary dark:text-dark-primary mb-4'>
+                                <FontAwesomeIcon icon = {faMapMarkerAlt} className='mr-3 text-light-warning dark:text-dark-warning'/>
+                                Descripcion
+                            </h3>
+                            <p className='text-light-text dark:text-dark-secondary'>
+                                {school.escuela.descripcion || 'Descripcion no disponible por el momento'} 
+                            </p>
+                        </section>
+
+
+                </div>
+            </div>
+        </main>
+    </div>
+);
+
+};
+
+export default SchoolDetail;

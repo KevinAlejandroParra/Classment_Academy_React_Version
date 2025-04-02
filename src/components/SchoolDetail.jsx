1a702b386f5d4963b078f7d61eaaca1daa5d85ae
Sneither <danielbernalg.04@gmail.com>
2025-04-01 21:24:44 -0500

    feature: school detail

---
+++ src/components/SchoolDetail.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faSchool, 
+  faUserTie, 
+  faCalendarAlt, 
+  faCalendarCheck, 
+  faUsers, 
+  faChild, 
+  faMapMarkerAlt, 
+  faPhone,
+  faClock,
+  faLocationDot,
+  faChalkboardUser
+} from '@fortawesome/free-solid-svg-icons';
+import CourseModel from '../models/CourseModel';

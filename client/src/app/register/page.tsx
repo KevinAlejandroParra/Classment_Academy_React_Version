ad9dae966d27e6c387fc5766df1a1511390a2bcf
Sneither <danielbernalg.04@gmail.com>
2025-04-05 16:43:57 -0500

    chore: register homepage

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -3,7 +3,7 @@ import React, { useState, ChangeEvent, FormEvent } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import  Link from "next/link" ;
 import { JSX } from "react";
-
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import {
   faUser,
   faIdCard,
@@ -12,7 +12,6 @@ import {
   faPhone,
   faMapMarkerAlt,
   faCalendar,
-  faHome,
 } from "@fortawesome/free-solid-svg-icons";
 
 interface FormData {

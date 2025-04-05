93ac03177e6700de1e5361d9b86339dd8f7a2de1
Sneither <danielbernalg.04@gmail.com>
2025-04-05 16:43:07 -0500

    chore: routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,11 +3,20 @@ const UserController = require("../controllers/userController.js");
 
 const userRoutes = Router();
 
+//obtener todos los usuarios
 userRoutes.get("/users", UserController.getUsers);
+
+//filtrar por id
 userRoutes.get("/users/:id", UserController.getUser);
+
+//register
 userRoutes.post("/users", UserController.createUser);
+
+//update and delete
 userRoutes.put("/users/:id", UserController.updateUser);
 userRoutes.delete("/users/:id", UserController.deleteUser);
+
+//login
 userRoutes.post("/login", UserController.login);
 
 

aea3c128cf5bafb8eaeb55c087f77f28ea079cf1
KevinAlejandroParra <luisparra5380@gmail.com>
2025-06-10 02:52:04 -0500

    feat: add react-hot-toast dependency and update email configuration error handling

--- server/src/config/emailConfig.js
+++ server/src/config/emailConfig.js
@@ -2,13 +2,16 @@ const nodemailer = require('nodemailer');
 
 // Crear el transporter para Gmail
 const createTransporter = () => {
+    if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
+        console.error('CREDENCIALES DE EMAIL CON FALLOS');
+        throw new Error('Email configuration is missing');
+    }
+
     return nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true para puerto 465, false para otros puertos
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            pass: process.env.EMAIL_PASSWORD
         }
     });
 };
